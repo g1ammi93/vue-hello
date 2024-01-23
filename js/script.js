91f4e4841cd7785b2a4f77b86aea70b830822fde
g1ammi93 <giammariacarelli93@hotmail.it>
2024-01-23 20:49:46 +0000

    Added Img

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const { createApp } = Vue;
 const app = createApp({
     data() {
         return {
-            saluto: 'Ciao a tutti i visitatori'
+            saluto: 'Ciao a tutti i visitatori',
+            imgHello: 'https://img.freepik.com/free-vector/hand-drawn-flat-design-people-waving-illustration_23-2149195759.jpg?size=626&ext=jpg&ga=GA1.1.632798143.1705881600&semt=sph'
         }
     }
 })
