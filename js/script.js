94fce3e70fb93b1ac4edc7a7bf9dc99efb519780
g1ammi93 <giammariacarelli93@hotmail.it>
2024-01-23 19:50:01 +0000

    Added Text

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ console.log('Vue ok', Vue)
 const { createApp } = Vue;
 const app = createApp({
     data() {
-
+        return {
+            saluto: 'Ciao a tutti i visitatori'
+        }
     }
 })
-app.mount('#root')
+app.mount('#root');
